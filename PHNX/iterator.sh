95b95cfb06482bbc857436fdf1431570ef707c88
Dmytro Onishchenko <tameter1917@yahoo.com>
2018-09-23 15:55:51 -0500

    Add inner restart feature

--- PHNX/iterator.sh
+++ PHNX/iterator.sh
@@ -9,10 +9,16 @@ SUPERTASK=$1
 INTERVAL=$2
 USER=$3
 MAX_PARALLEL_JOBS=$4
+RUNTIME_LIMIT=$5
+
 ALL_TASKS="$SUPERTASK"/ALL_TASKS
 DONE_TASKS="$SUPERTASK"/DONE_TASKS
 ERR_TASKS="$SUPERTASK"/ERR_TASKS
 
+start_time=`date '+%s'`
+
+RUNTIME_SECS=$(($RUNTIME_LIMIT * 60 * 60))
+TIME_TO_STOP=$(($RUNTIME_SECS - ($INTERVAL * 3)))
 echo " >> Iterator started"
 echo " >> New iteration each $INTERVAL seconds"
 
@@ -21,6 +27,13 @@ echo " >> New iteration each $INTERVAL seconds"
 
 while true 
 do
+    # # Check if the 35-hours runtime limit comes up
+    current_time=`date '+%s'`
+    time_passed=$(($current_time - $start_time))
+    if [ $time_passed -gt $RUNTIME_LIMIT ] ; then
+        ./$0 $1 $2 $3 $4 $5
+        break
+    fi
     let TIME="$INTERVAL"
     sleep $TIME
     TOTAL=`file_count $ALL_TASKS`
