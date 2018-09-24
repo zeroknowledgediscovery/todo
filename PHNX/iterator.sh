cf30dd4b5f1aa74c213f278a342569743b2e6bbd
Dmytro Onishchenko <tameter1917@yahoo.com>
2018-09-24 12:28:51 -0500

    Iterator now restarts upon each iteration

--- PHNX/iterator.sh
+++ PHNX/iterator.sh
@@ -9,7 +9,6 @@ SUPERTASK=$1
 INTERVAL=$2
 USER=$3
 MAX_PARALLEL_JOBS=$4
-RUNTIME_LIMIT=$5
 
 ALL_TASKS="$SUPERTASK"/ALL_TASKS
 DONE_TASKS="$SUPERTASK"/DONE_TASKS
@@ -17,40 +16,28 @@ ERR_TASKS="$SUPERTASK"/ERR_TASKS
 
 start_time=`date '+%s'`
 
-RUNTIME_SECS=$(($RUNTIME_LIMIT * 60 * 60))
-TIME_TO_STOP=$(($RUNTIME_SECS - ($INTERVAL * 3)))
 echo " >> Iterator started"
 echo " >> New iteration each $INTERVAL seconds"
 
-# Initial launch
-./supertask_run.sh $SUPERTASK $USER $MAX_PARALLEL_JOBS
 
-while true 
-do
-    # # Check if the 35-hours runtime limit comes up
-    current_time=`date '+%s'`
-    time_passed=$(($current_time - $start_time))
-    if [ $time_passed -gt $RUNTIME_LIMIT ] ; then
-        exec $0 $1 $2 $3 $4 $5
-        break
-    fi
-    let TIME="$INTERVAL"
-    sleep $TIME
-    TOTAL=`file_count $ALL_TASKS`
-    DONE=`file_count $DONE_TASKS`
-    ERR=`file_count $ERR_TASKS`
-    # TRACK HOW MANY TASKS WERE PROCESSED - EITHER SUCCESSFULLY OR NOT
-    let PROCESSED=`expr $DONE + $ERR`
+TOTAL=`file_count $ALL_TASKS`
+DONE=`file_count $DONE_TASKS`
+ERR=`file_count $ERR_TASKS`
+# TRACK HOW MANY TASKS WERE PROCESSED - EITHER SUCCESSFULLY OR NOT
+let PROCESSED=`expr $DONE + $ERR`
 
-    # STOP ITERATOR IF ALL TASKS WERE PROCESSED
-    if [ $PROCESSED -lt $TOTAL ] ; then
-        ./supertask_run.sh $SUPERTASK $USER $MAX_PARALLEL_JOBS
-    else 
-        echo "============================================"
-        echo "========= SUPERTASK RUN COMPLETE ==========="
-        echo "============================================"
-        echo "| complete: $DONE"
-        echo "| erred:    $ERR"
-        break
-    fi
-done
+# STOP ITERATOR IF ALL TASKS WERE PROCESSED
+if [ $PROCESSED -lt $TOTAL ] ; then
+    ./supertask_run.sh $SUPERTASK $USER $MAX_PARALLEL_JOBS
+else 
+    echo "============================================"
+    echo "========= SUPERTASK RUN COMPLETE ==========="
+    echo "============================================"
+    echo "| complete: $DONE"
+    echo "| erred:    $ERR"
+    exit 1
+fi
+let TIME="$INTERVAL"
+sleep $TIME
+exec $0 $1 $2 $3 $4 $5
+
